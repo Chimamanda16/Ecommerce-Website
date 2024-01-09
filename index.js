f48707bc2e406972624df9fd1e9510ec1809723d
Chimamanda16 <chimamandaunachukwu@gmail.com>
2024-01-09 20:46:26 +0100

    Add total price function

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 // Variables and constants
 const cart = document.querySelector(".cart");
+var cartBox = document.getElementsByClassName("cart-box");
 const cartContent = document.querySelector(".cart-content");
 const cartQuantity = document.querySelector(".cart-quantity");
 const cartIcon = document.querySelector("#cart-icon");
 const closeCartBtn = document.querySelector(".close-cart");
 const delItemBtn = document.querySelector(".cart-remove");
 const nav = document.querySelector(".nav");
+const totalPrice = document.querySelector(".total-price");
 
 // Cart open and close
 const closeCart = ()=>{
@@ -24,13 +26,21 @@ closeCartBtn.addEventListener("click", closeCart)
 
 // Keep item quantity positive
 const positive = () =>{
-    if(cartQuantity.value <= 0 || isNaN(cartQuantity)){
+    if(cartQuantity.value <= 0){
         cartQuantity.value = 1;
     }
 }
 cartQuantity.addEventListener("click", positive);
 
 // Update Total Price
+const updatePrice = () =>{
+    // Price tag
+    const priceTag = cartQuantity.previousElementSibling.innerHTML;
+    var price = priceTag.match(/\d+/);
+    price = Math.round((cartQuantity.value * price)*100)/100;
+    totalPrice.innerHTML = "$" + price; 
+}
+cartQuantity.addEventListener("click", updatePrice);
 
 // Remove item from cart
 if (document.readyState === "loading") {
