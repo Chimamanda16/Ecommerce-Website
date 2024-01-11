36e0e36df005cf29305123bebe8eec4efb59ebf5
Chimamanda16 <chimamandaunachukwu@gmail.com>
2024-01-11 03:17:07 +0100

    Add price update function

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var cartContent = document.querySelector(".cart-content");
 const cartIcon = document.querySelector("#cart-icon");
 const closeCartBtn = document.querySelector(".close-cart");
 const nav = document.querySelector(".nav");
+const cartTotalPrice = document.querySelector(".total-price");
 
 // Cart open and close
 const closeCart = ()=>{
@@ -20,13 +21,31 @@ const openCart = ()=>{
 cartIcon.addEventListener("click", openCart);
 closeCartBtn.addEventListener("click", closeCart)
 
-// Keep item quantity positive
-function UpdateQuantityNumber(cartQuant){
-    if(cartQuant.target.value <= 0){
-        cartQuant.target.value = 1;
+// Update Total
+function updateTotal(){
+    var cartBoxes = cartContent.getElementsByClassName("cart-box");
+    var totalPrice = 0;
+    for(var i = 0; i<cartBoxes.length; i++){
+        var priceTag = cartBoxes[i].getElementsByClassName("cart-price")[0].innerHTML;
+        var cartBoxQuant = cartBoxes[i].getElementsByClassName("cart-quantity")[0].value;
+        var cartBoxPrice = priceTag.match(/\d+/);
+        price = Math.round((cartBoxQuant * cartBoxPrice)*100)/100;
+        totalPrice += price;
+        cartTotalPrice.innerHTML = "$" + totalPrice;
+    }
+    if(cartBoxes.length == 0){
+        cartTotalPrice.innerHTML = "$" + 0;
     }
 }
 
+// Keep item quantity positive
+function UpdateQuantityNumber(cartQuant){
+    updateTotal();
+    if(cartQuant.target.value <= 0){
+        cartQuant.target.value = 1;
+        updateTotal();
+    }
+}
 // Add to cart function
 function getItemInfo(event){
     var parentElement = event.parentNode;
@@ -40,7 +59,7 @@ function getItemInfo(event){
     <input type="number" name="" value="1" class="cart-quantity">
     <i class="fa-solid fa-trash cart-remove" style="color: red;"></i>
     </div>`;
-    
+
     function createItem(img, title, pricing){
         var cartShopBox = document.createElement("div");
         cartShopBox.classList.add("cart-box");
@@ -65,6 +84,7 @@ function getItemInfo(event){
             delItemBtn.addEventListener("click", function(event){
                 const cartRemove = delItemBtn.parentNode.parentNode;
                 cartRemove.remove();
+                updateTotal();
             });
         }
         else{
@@ -80,6 +100,7 @@ function getItemInfo(event){
             delItemBtn.addEventListener("click", function(event){
                 const cartRemove = delItemBtn.parentNode.parentNode;
                 cartRemove.remove();
+                updateTotal();
             });
         }
     }
@@ -89,5 +110,6 @@ addCart.forEach(function(button){
     button.addEventListener("click", function(event){
         var addCartIcon = event.currentTarget;
         getItemInfo(addCartIcon);
+        updateTotal();
     });
 });
